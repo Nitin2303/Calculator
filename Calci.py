b14d669b882a056f0509a4a0cbf55f0191bdcec7
Nitin2303 <82812006+Nitin2303@users.noreply.github.com>
2022-03-29 22:10:56 +0530

    Add files via upload

---
+++ Calci.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+from tkinter import *
+console = tk.Tk()
+console.geometry("170x250")
+console.title("python-calculator")
+console.maxsize(170,250)
+console.minsize(170,250)
+ent = tk.Entry(console, width=16, borderwidth=3, relief=tk.RIDGE)
+ent.grid(pady=10,row=0,sticky="w",padx=15)
+
+
+def delete():
+    a = ent.get()
+    ent.delete(first=len(a) - 1, last="end")
+
+
+def fresult():
+    if ent.get() == "":
+        pass
+    elif ent.get()[0] == "0":
+        ent.delete(0, "end")
+    else:
+        c_res = ent.get()
+        c_res = eval(c_res)
+        clearf()
+        ent.insert("end", c_res)
+
+
+def clearf():
+    ent.delete(0, "end")
+clean = Button(console,text="C",width=2,command=clearf,bg="green",fg="white",relief=RIDGE)
+clean.grid(row=0,sticky="w",padx=125)
+Char_nine = Button(text="9",width=2,command=lambda : ent.insert("end","9"),borderwidth=3,relief=RIDGE)
+Char_nine.grid(row=1,sticky="w",padx=15)
+Char_eight = Button(text="8",width=2,command=lambda : ent.insert("end","8"),borderwidth=3,relief=RIDGE)
+Char_eight.grid(row=1,sticky="w",padx=45)
+Char_seven = Button(console,text="7",width=2,command=lambda : ent.insert("end","7"),borderwidth=3,relief=RIDGE)
+Char_seven.grid(row=1,sticky="w",padx=75)
+Char_plus = Button(console,text="+",width=2,command=lambda : ent.insert("end","+"),borderwidth=3,relief=RIDGE)
+Char_plus.grid(row=1,sticky="e",padx=125)
+Char_six = Button(text="6",width=2,command=lambda : ent.insert("end","6"),borderwidth=3,relief=RIDGE)
+Char_six.grid(row=2,sticky="w",padx=15,pady=5)
+Char_five = Button(text="5",width=2,command=lambda : ent.insert("end","5"),borderwidth=3,relief=RIDGE)
+Char_five.grid(row=2,sticky="w",padx=45,pady=5)
+Char_four = Button(console,text="4",width=2,command=lambda : ent.insert("end","4"),borderwidth=3,relief=RIDGE)
+Char_four.grid(row=2,sticky="w",padx=75,pady=5)
+Char_minus = Button(console,text="-",width=2,command=lambda : ent.insert("end","-"),borderwidth=3,relief=RIDGE)
+Char_minus.grid(row=2,sticky="e",padx=125,pady=5)
+Char_three = Button(text="3",width=2,command=lambda : ent.insert("end","3"),borderwidth=3,relief=RIDGE)
+Char_three.grid(row=3,sticky="w",padx=15,pady=5)
+Char_two = Button(text="2",width=2,command=lambda : ent.insert("end","2"),borderwidth=3,relief=RIDGE)
+Char_two.grid(row=3,sticky="w",padx=45,pady=5)
+Char_one = Button(console,text="1",width=2,command=lambda : ent.insert("end","1"),borderwidth=3,relief=RIDGE)
+Char_one.grid(row=3,sticky="w",padx=75,pady=5)
+Char_multiply = Button(console,text="*",width=2,command=lambda : ent.insert("end","*"),borderwidth=3,relief=RIDGE)
+Char_multiply.grid(row=3,sticky="e",padx=125,pady=5)
+Char_zero = Button(text="0",width=2,command=lambda : ent.insert("end","0"),borderwidth=3,relief=RIDGE)
+Char_zero.grid(row=4,sticky="w",padx=15,pady=5)
+Char_double_zero = Button(text="00",width=2,command=lambda : ent.insert("end","00"),borderwidth=3,relief=RIDGE)
+Char_double_zero.grid(row=4,sticky="w",padx=45,pady=5)
+Char_dot = Button(console,text=".",width=2,command=lambda : ent.insert("end","."),borderwidth=3,relief=RIDGE)
+Char_dot.grid(row=4,sticky="w",padx=75,pady=5)
+Char_divide = Button(console,text="/",width=2,command=lambda : ent.insert("end","/"),borderwidth=3,relief=RIDGE)
+Char_divide.grid(row=4,sticky="e",padx=125,pady=5)
+result = Button(console,text="=",width=10,command=fresult,bg="green",fg="white",borderwidth=3,relief=RIDGE)
+result.grid(row=5,sticky="w",padx=15,pady=5)
+Char_modulus = Button(console,text="%",width=2,command=lambda : ent.insert("end","%"),borderwidth=3,relief=RIDGE)
+Char_modulus.grid(row=5,sticky="e",padx=125,pady=5)
+delete = Button(console,text="del",width=2,command=delete,borderwidth=3,relief=RIDGE)
+delete.grid(row=5,sticky="w",padx=80,pady=5)
+la = Label(console,text="Developed by Nithiin")
+la.place(relx=0.0,rely=1.0,anchor="sw")
+
+
+console.mainloop()
